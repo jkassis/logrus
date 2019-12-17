32d5f474021b2c7fe43eb4f28568e3fd47a81ce7
Jeremy Kassis <jkassis@gmail.com>
2019-12-17 08:35:08 -0800

    add adapter for HCLog format

---
+++ adapterHCLog.go
@@ -0,0 +1,148 @@
+package logrus
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+)
+
+// AdapterHCLog implements the hclog interface, and wraps it
+// around a Logrus entry
+type AdapterHCLog struct {
+	Log  FieldLogger
+	name string
+}
+
+// HCLog has one more level than we do. As such, we will never
+// set trace level.
+func (*AdapterHCLog) Trace(_ string, _ ...interface{}) {
+	return
+}
+
+func (a *AdapterHCLog) Debug(msg string, args ...interface{}) {
+	a.CreateEntry(args).Debug(msg)
+}
+
+func (a *AdapterHCLog) Info(msg string, args ...interface{}) {
+	a.CreateEntry(args).Info(msg)
+}
+
+func (a *AdapterHCLog) Warn(msg string, args ...interface{}) {
+	a.CreateEntry(args).Warn(msg)
+}
+
+func (a *AdapterHCLog) Error(msg string, args ...interface{}) {
+	a.CreateEntry(args).Error(msg)
+}
+
+func (a *AdapterHCLog) IsTrace() bool {
+	return false
+}
+
+func (a *AdapterHCLog) IsDebug() bool {
+	return a.shouldEmit(DebugLevel)
+}
+
+func (a *AdapterHCLog) IsInfo() bool {
+	return a.shouldEmit(InfoLevel)
+}
+
+func (a *AdapterHCLog) IsWarn() bool {
+	return a.shouldEmit(WarnLevel)
+}
+
+func (a *AdapterHCLog) IsError() bool {
+	return a.shouldEmit(ErrorLevel)
+}
+
+func (a *AdapterHCLog) SetLevel(hclog.Level) {
+	// interface definition says it is ok for this to be a noop if
+	// implementations don't need/want to support dynamic level changing, which
+	// we don't currently.
+}
+
+func (a *AdapterHCLog) With(args ...interface{}) hclog.Logger {
+	e := a.CreateEntry(args)
+	return &AdapterHCLog{Log: e}
+}
+
+func (a *AdapterHCLog) ImpliedArgs() []interface{} {
+	return nil
+}
+
+func (a *AdapterHCLog) Name() string {
+	return a.name
+}
+
+func (a *AdapterHCLog) Named(name string) hclog.Logger {
+	var newName bytes.Buffer
+	if a.name != "" {
+		newName.WriteString(a.name)
+		newName.WriteString(".")
+	}
+	newName.WriteString(name)
+
+	return a.ResetNamed(newName.String())
+}
+
+func (a *AdapterHCLog) ResetNamed(name string) hclog.Logger {
+	fields := []interface{}{"subsystem_name", name}
+	e := a.CreateEntry(fields)
+	return &AdapterHCLog{Log: e, name: name}
+}
+
+// StandardLogger is meant to return a stdlib Logger type which wraps around
+// hclog. It does this by providing an io.Writer and instantiating a new
+// Logger. It then tries to interpret the log level by parsing the message.
+//
+// Since we are not using `hclog` in a generic way, and I cannot find any
+// calls to this method from go-plugin, we will poorly support this method.
+// Rather than pull in all of hclog writer parsing logic, pass it a Logrus
+// writer, and hardcode the level to INFO.
+//
+// Apologies to those who find themselves here.
+func (a *AdapterHCLog) StandardLogger(opts *hclog.StandardLoggerOptions) *log.Logger {
+	entry := a.Log.WithFields(Fields{})
+	return log.New(entry.WriterLevel(InfoLevel), "", 0)
+}
+
+func (a *AdapterHCLog) StandardWriter(opts *hclog.StandardLoggerOptions) io.Writer {
+	var w io.Writer
+	logger, ok := a.Log.(*Logger)
+	if ok {
+		w = logger.Out
+	}
+	if w == nil {
+		w = os.Stderr
+	}
+	return w
+}
+
+func (a *AdapterHCLog) shouldEmit(level Level) bool {
+	currentLevel := a.Log.WithFields(Fields{}).Level
+	if currentLevel >= level {
+		return true
+	}
+
+	return false
+}
+
+func (a *AdapterHCLog) CreateEntry(args []interface{}) *Entry {
+	if len(args)%2 != 0 {
+		args = append(args, "<unknown>")
+	}
+
+	fields := make(Fields)
+	for i := 0; i < len(args); i = i + 2 {
+		k, ok := args[i].(string)
+		if !ok {
+		}
+		v := args[i+1]
+		fields[k] = v
+	}
+
+	return a.Log.WithFields(fields)
+}
--- go.mod
+++ go.mod
@@ -1,11 +1,10 @@
 module github.com/sirupsen/logrus
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/hashicorp/go-hclog v0.10.0
 	github.com/konsorten/go-windows-terminal-sequences v1.0.1
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.2.2
-	golang.org/x/sys v0.0.0-20190422165155-953cdadca894
+	golang.org/x/sys v0.0.0-20191008105621-543471e840be
 )
 
 go 1.13
